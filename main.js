517eb17085eb691eca0060b1c6c82970d77380d6
Cherubim <git@cherubimdev.mozmail.com>
2024-12-27 16:26:09 +0000

    Expanded grid to 16x16

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // TODO Autofire this function upon the page being fully loaded.
+
+const numberOfCells = 16 * 16;
+
 function createCanvas() {
     let canvas = document.getElementById("canvas")
-    for (let i=0; i<16; i++) {
+    for (let i=0; i<numberOfCells; i++) {
         let newDiv = document.createElement("div")
         newDiv.setAttribute("class", "cell")
         newDiv.setAttribute("id", "cell" + i)
+        newDiv.innerText = i
         canvas.appendChild(newDiv)
     }
     document.getElementById("createbutton").setAttribute("disabled", "")
