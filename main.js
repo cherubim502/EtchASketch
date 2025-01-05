d87b4d17e4a7c41297ee4f94786dde88f6a32336
Cherubim <git@cherubimdev.mozmail.com>
2025-01-04 21:30:56 -0500

    Centered everything

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const numberOfCells = 16 * 16;
-
-// Fill the canvas based on the 
+// Fill the canvas based on the input value given by the user.
 function createCanvas() {
-    // 
+    // Get the number of cells per row and square it
+    let cellsPerRow = document.getElementById("cellcount").value
+    let numberOfCells = cellsPerRow * cellsPerRow
 
     let canvas = document.getElementById("canvas")
     for (let i=0; i<numberOfCells; i++) {
         let newDiv = document.createElement("div")
         newDiv.setAttribute("class", "cell")
         newDiv.setAttribute("id", "cell" + i)
-        newDiv.innerText = i
         newDiv.addEventListener("mouseover", setDivDark)
-        newDiv.setAttribute("style","width:" + (1000/16))
-        newDiv.style.width = 1000 / 16
+        newDiv.setAttribute("style","width:" + (1000/cellsPerRow) + "px")
+        newDiv.style.width = 1000 / cellsPerRow
         canvas.appendChild(newDiv)
     }
     document.getElementById("createbutton").setAttribute("disabled", "")
 }
 
-function setDivDark(){
+// Set the div that fired this event to the background color "grey"
+function setDivDark() {
     this.style.backgroundColor = 'grey';
 }
