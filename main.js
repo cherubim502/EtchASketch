98c8c9f5547998574794df5e540c39aa59838125
Cherubim <git@cherubimdev.mozmail.com>
2025-01-05 01:00:16 +0000

    got onmouseover event working

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-// TODO Autofire this function upon the page being fully loaded.
-
 const numberOfCells = 16 * 16;
 
+// Fill the canvas based on the 
 function createCanvas() {
+    // 
+
     let canvas = document.getElementById("canvas")
     for (let i=0; i<numberOfCells; i++) {
         let newDiv = document.createElement("div")
         newDiv.setAttribute("class", "cell")
         newDiv.setAttribute("id", "cell" + i)
         newDiv.innerText = i
+        newDiv.addEventListener("mouseover", setDivDark)
+        newDiv.setAttribute("style","width:" + (1000/16))
+        newDiv.style.width = 1000 / 16
         canvas.appendChild(newDiv)
     }
     document.getElementById("createbutton").setAttribute("disabled", "")
+}
+
+function setDivDark(){
+    this.style.backgroundColor = 'grey';
 }
